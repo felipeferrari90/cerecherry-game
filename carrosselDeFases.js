35ab0c7210705112a416c2b311ffcc2b2498d4f7
Felipe Ferreira <53275466+felipeferrari90@users.noreply.github.com>
2022-07-19 14:10:57 -0300

    Add files via upload
    mvp-world-1

---
+++ carrosselDeFases.js
@@ -0,0 +1,6 @@
+import fase1 from './fases/fase1';
+import fase2 from './fases/fase2';
+import fase3 from './fases/fase3';
+
+export default fase2;
+
---
+++ index.js
@@ -0,0 +1,10 @@
+import fase from './fases/fase2'
+
+
+'use strict mode';
+fase.renderFase();
+document.body.addEventListener('keydown',fase.movePerson.bind(fase),false);
+
+
+
+
