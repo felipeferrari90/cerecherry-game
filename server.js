624c409ac014ac77c004e6203f0ad34862bb6051
Felipe Ferreira <53275466+felipeferrari90@users.noreply.github.com>
2022-07-21 14:20:27 -0300

    agora vai mesmo

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const router = express.Router();
-const fs = require('fs');
 
 router.get('/',function(req,res){
   res.header('Cache-Control', 'no-cache, no-store, must-revalidate')
   res.set({
     'Content-Type': 'text/html',
   })
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 app.use('/fase', router)
