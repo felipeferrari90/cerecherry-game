7a2a68b228e5579a228c2e7b5ac4bc0b5acbfbb4
Felipe Ferreira <53275466+felipeferrari90@users.noreply.github.com>
2022-07-22 13:29:43 -0300

    testes com now

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const app = require("./server")
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+app.get('/',function(req,res){
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.set({
+    'Content-Type': 'text/html',
+  })
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+
 app.listen(8000);
 
